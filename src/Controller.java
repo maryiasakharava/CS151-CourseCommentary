cd213af0d77a294110743f4282d8ce11db4f1f6a
anusrichavali <64172984+anusrichavali@users.noreply.github.com>
2023-05-07 20:29:44 -0700

    Delete src directory

--- src/Controller.java
+++
@@ -1,7 +0,0 @@
-//Controller class which is run in order to generate the app
-
-public class Controller {
-    public static void main(String[] args) {
-        CourseCommentary h = new CourseCommentary();
-    }
-}
